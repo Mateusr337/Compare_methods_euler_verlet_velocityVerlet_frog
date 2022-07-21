15f4b57b426f41a0bc9b5f462b2cbd5fb7839df8
Mateus <mateusc.rossetto@hotmail.com>
2022-07-21 15:02:15 -0300

    feat velocity verlet

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pyexpat.errors import XML_ERROR_UNEXPECTED_STATE
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -21,7 +22,6 @@ def Euler(t_max, delta_t, v0, x0, a):
     xt = x0
 
     for t in np.arange(delta_t, t_max, delta_t):
-
         new_v = vt + a(xt) * delta_t
         new_x = xt + vt * delta_t
 
@@ -60,15 +60,65 @@ def Verlet(t_max, delta_t, x0, v0, a):
     return {"x": position, "t": time, "v": velocity}
 
 
-euler = Euler(t_max, delta_t, v0, x0, a)
-verlet = Verlet(t_max, delta_t, v0, x0, a)
+def Frog(t_max, delta_t, v0, x0, a):
+    v = [v0]
+    x = [x0]
+    t_interval = [0]
+
+    vt_2 = v0 + a(x0) * (delta_t/2)
+    xt = x0
+
+    for t in np.arange(delta_t, t_max, delta_t):
+        x_next = xt + vt_2 * delta_t
+        v_next_2 = vt_2 + a(x_next) * (delta_t)
+
+        v.append((v_next_2 + vt_2) / 2)
+        x.append(x_next)
+        t_interval.append(t)
+
+        vt_2 = v_next_2
+        xt = x_next
+
+    return {"v": v, "x": x, 't': t_interval}
 
 
-plt.plot(euler["t"], euler["v"])
-plt.plot(euler["t"], euler["x"])
+def Velocity_verlet(t_max, delta_t, v0, x0, a):
+    v = [v0]
+    x = [x0]
+    t_interval = [0]
 
-plt.plot(verlet["t"], verlet["v"])
-plt.plot(verlet["t"], verlet["x"])
+    vt = v0
+    xt = x0
 
-plt.legend(["euler v", "euler x", "verlet v", "verlet x"])
-plt.show()
+    for t in np.arange(delta_t, t_max, delta_t):
+        vt_2 = vt + a(xt) * delta_t / 2
+
+        next_x = xt + vt_2 * delta_t
+        next_v = vt_2 + a(next_x) * delta_t / 2
+
+        v.append(next_v)
+        x.append(next_x)
+        t_interval.append(t)
+
+        vt = next_v
+        xt = next_x
+
+    return {"v": v, "x": x, 't': t_interval}
+
+
+# euler = Euler(t_max, delta_t, v0, x0, a)
+frog = Frog(t_max, delta_t, v0, x0, a)
+verlet = Verlet(t_max, delta_t, x0, v0, a)
+
+
+# plt.plot(euler["t"], euler["v"])
+# plt.plot(euler["t"], euler["x"])
+
+# plt.plot(verlet["t"], verlet["v"])
+# plt.plot(verlet["t"], verlet["x"])
+
+# plt.plot(frog["t"], frog["v"])
+# plt.plot(frog["t"], frog["x"])
+
+# plt.legend(["euler v", "euler x", "verlet v", "verlet x", "frog v", "frog x"])
+# plt.show()
